300fc6f99d04e54c87c63f810a5ac16c58d7c1c1
Chilet-Okeke <okekechiletaram@gmail.com>
2024-08-15 14:34:58 +0100

    new fixes

--- app/(Home)/_components/index.jsx
+++ app/(Home)/_components/index.jsx
@@ -1,13 +1,14 @@
 "use client";
+import faceIO from "@faceio/fiojs";
 import { Camera, Lock } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 const HomeIndex = () => {
   return (
     <div className="w-full">
       <Navbar />
-      <MainContent/>
-      <Footer/>
+      <MainContent />
+      <Footer />
     </div>
   );
 };
@@ -52,7 +53,23 @@ const Navbar = () => {
 };
 
 const MainContent = () => {
+  const [isauthenticated, setIsAuthenticated] = useState(false)
+  const faceio = new faceIO(process.env.NEXT_PUBLIC_KEY);
 
+  const AuthenticateUser = async () => {
+    try {
+      const userData = await faceio.authenticate({
+        locale: "auto",
+      });
+      console.log("Success, user identified");
+      console.log("Linked facial Id: " + userData.facialId);
+      console.log("Payload: " + JSON.stringify(userData.payload));
+      localStorage.setItem("faceId", userData.facialId); // Store face ID in local storage
+      // router.push("");
+    } catch (error) {
+      console.error("Authentication failed:", error);
+    }
+  };
   return (
     <div className="w-full min-h-[100vh] items-center justify-center py-6 flex flex-col gap-4">
       <div className="w-[90%] rounded-2xl md:w-[500px] mx-auto border p-8 flex flex-col gap-8 ">
@@ -76,7 +93,7 @@ const MainContent = () => {
 
           <div className="text-base flex flex-col gap-2 font-semibold">
             Facial Authentication
-            <div className="w-full bg-[#FBF6F7] rounded-md flex items-center justify-center text-2xl h-[300px]">
+            <div onClick={AuthenticateUser} className="w-full bg-[#FBF6F7] rounded-md flex items-center justify-center text-2xl h-[300px]">
               <div className="w-[60px] bg-[#fff] shadow-sm rounded-lg flex items-center justify-center text-2xl h-[60px]">
                 <Camera />
               </div>
@@ -84,7 +101,7 @@ const MainContent = () => {
             <span className="block text-sm font-normal" >
               Hold your face in front of the camera for facial authentication.
             </span>
-            <div className="p-3 hover:opacity-[.6] cursor-pointer px-4 rounded-sm text-base text-center font-semibold text-[#fff] bg-[#000]">
+            <div onClick={authenticateUser} className="p-3 hover:opacity-[.6] cursor-pointer px-4 rounded-sm text-base text-center font-semibold text-[#fff] bg-[#000]">
               Take Photo
             </div>
           </div>
@@ -92,7 +109,9 @@ const MainContent = () => {
       </div>
 
       {/* community voting */}
-      <div className="w-[90%] rounded-2xl md:w-[500px] mx-auto border p-8 flex flex-col gap-8 ">
+      {/* check if the user has been authenticated */}
+      {
+        isauthenticated && <div className="w-[90%] rounded-2xl md:w-[500px] mx-auto border p-8 flex flex-col gap-8 ">
         <span className="text-3xl font-bold gap-3 text-[#000]">
           Community Voting
           <span className="block font-normal text-base text-grey">
@@ -137,6 +156,8 @@ const MainContent = () => {
           </div>
         </div>
       </div>
+      }
+      
     </div>
   );
 };
